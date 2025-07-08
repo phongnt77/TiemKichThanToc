d1edff472ea4306e235370ffbfe33766e5cd90e2
andev03 <nguyenhoangan03.work@gmail.com>
2025-07-08 22:26:25 +0700

    feat: add introduce

---
+++ Assets/Scripts/VideoEndSceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoEndSceneLoader : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        StartCoroutine(DelayAndLoadScene());
+    }
+
+    IEnumerator DelayAndLoadScene()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(1);
+    }
+}
