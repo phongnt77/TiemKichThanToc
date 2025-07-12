1e68868d7cd78dd9cf6461b5051ae71892bcd15b
tinht <cinitcv@gmail.com>
2025-07-12 17:19:34 +0700

    feat: add some code

---
+++ Assets/Scripts/Endless/LoadingController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Enless
+{
+    public static class LoadingData
+    {
+        public static string SceneToLoad;
+    }
+
+    public class LoadingController : MonoBehaviour
+    {
+        public Slider loadingBar; 
+
+        void Start() 
+        { 
+            StartCoroutine(LoadSceneAsync(LoadingData.SceneToLoad)); 
+        }
+
+        IEnumerator LoadSceneAsync(string sceneName)
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+            asyncLoad.allowSceneActivation = false;
+            while (asyncLoad.progress < 0.7f)
+            {
+                loadingBar.value = asyncLoad.progress;
+                yield return null;
+            }
+            float fakeProgress = 0.5f;
+            while (fakeProgress < 1f)
+            {
+                fakeProgress += Time.deltaTime * 0.5f;  // Tốc độ tăng slider (tuỳ chỉnh)
+                loadingBar.value = fakeProgress;
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.2f); // Cho cảm giác mượt thêm chút
+            asyncLoad.allowSceneActivation = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Endless/LoadingScreenManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+#region No development and use on this script, just try a loading screen for Endless Mode
+
+public class LoadingScreenManager : MonoBehaviour
+{
+    public static LoadingScreenManager Instance;
+    public GameObject loadingScreenObject;
+    public Slider loadingBar;
+    private void Awake()
+    {
+       if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void LoadEndlessMode()
+    {
+        loadingScreenObject.SetActive(true);
+        StartCoroutine(LoadSceneAsync("ScenceEndLess"));
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoad.allowSceneActivation = false;
+
+        while (asyncLoad.progress < 0.9f)
+        {
+            loadingBar.value = asyncLoad.progress;
+            yield return null;
+        }
+
+        loadingBar.value = 1f;
+        yield return new WaitForSeconds(0.3f);  // Cho cảm giác mượt
+        asyncLoad.allowSceneActivation = true;
+    }
+}
+#endregion
